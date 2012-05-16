d9cf19ef14c4a1d0401aa94a31a4b1728bbe66ac
thangvdmaster <thangvdmaster@80728436-8353-cb08-c207-d9da5290a688>
2012-05-16 10:40:19 +0000

    update comments

--- SourceCode/code MSSP/ADXL345_accel/ADXL345.h
+++ SourceCode/code MSSP/ADXL345_accel/ADXL345.h
@@ -4,4 +4,4 @@
 #define ADXL345_I2C 0x53
 
 unsigned char fufoInitAccel(void);//Initialize ADXL345
-unsigned char fufoReadAccel(int *data);//Read the values in an array of 3 integers, temp, accel_X, accel_Y, accel_Z. Return an error if it can not
+unsigned char fufoReadAccel(int *data);//Read the values in an array of 3 integers, accel_X, accel_Y, accel_Z. Return an error if it can not
