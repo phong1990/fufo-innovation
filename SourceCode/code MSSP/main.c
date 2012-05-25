edd264242e4ca28a01b4acd4247754a975a2d510
thangvdmaster <thangvdmaster@80728436-8353-cb08-c207-d9da5290a688>
2012-05-25 19:41:44 +0000

    update code for get data from accel or gyro

--- SourceCode/code MSSP/main.c
+++ SourceCode/code MSSP/main.c
@@ -5,6 +5,7 @@
 #include "Delay/DelayTMR1.h"
 #include "I2C/I2C.h"
 #include "LCD/LCD.h"
+#include "math.h"
 
 	_FOSC(CSW_FSCM_OFF & XT);		//Clock = 7.3728 MHz
 	_FWDT(WDT_OFF);
@@ -19,46 +20,152 @@ unsigned char buff[] = "abc";
 unsigned char str[16], temp;
 
 int main(void) {
-	 int dataGyroArray[3], dataAccelArray[3];
-
+	    unsigned int dataGyroArray[3], dataAccelArray[3];
+		unsigned char error = 0;
+		unsigned char noticeError[] = "Errored";
+		float xA, yA, zA, xG, yG, zG;
+		float convertAccel = 0.0039;
+		float convertGyro = 0.00875;
+        int xIntA, yIntA, zIntA, xIntG, yIntG, zIntG;
+		_RE0 = 0;
+		_TRISE0 = 1;
         fufoInitSystem();
         while(1)
-        {
-
-        	fufoReadAccel(dataAccelArray);
-        	fufoReadGyro(dataGyroArray);
-			fufoCmd4LCD(LCD_CLEAR);
-			fufoDelayMs(5000);
-//			_LATE8 = 1;
-//			fufoDelayMs(1000);
-//			_LATE8 = 0;
-//			fufoOutputChar(buff);
-
-			fufoOutputInt(dataAccelArray[0]);
-			fufoDelayMs(10);
-			fufoDat4LCD(',');
-			fufoDelayMs(10);
-			fufoOutputInt(dataAccelArray[1]);
-			fufoDelayMs(10);
-			fufoDat4LCD(',');
-			fufoDelayMs(10);
-			fufoOutputInt(dataAccelArray[2]);
-            fufoDelayMs(10);
-			fufoDelayMs(10000);
-			
-			fufoCmd4LCD(LCD_CLEAR);
-			fufoDelayMs(5);
-			fufoOutputInt(dataGyroArray[0]);
-			fufoDelayMs(10);
-			fufoDat4LCD(',');
-			fufoDelayMs(10);
-			fufoOutputInt(dataGyroArray[1]);
-			fufoDelayMs(10);
-			fufoDat4LCD(',');
-			fufoDelayMs(10);
-			fufoOutputInt(dataGyroArray[2]);
-            fufoDelayMs(10);
-			fufoDelayMs(10000);			
+        {	if(_RE0 == 0) {
+				error = fufoReadAccel(dataAccelArray);
+				if(error == 0) {
+		        	if(dataAccelArray[0] < 4096) {
+						xA = dataAccelArray[0] * convertAccel * 1000;	
+					} else {
+						xA = (8192 - dataAccelArray[0]) * convertAccel * 1000;
+					
+					}
+		
+					if(dataAccelArray[1] < 4096) {
+						yA = dataAccelArray[1] * convertAccel * 1000;	
+					} else {
+						yA = (8192 - dataAccelArray[1]) * convertAccel * 1000;
+							
+					}
+		
+					if(dataAccelArray[2] < 4096) {
+						zA = dataAccelArray[2] * convertAccel * 1000;	
+					} else {
+						zA = (8192 - dataAccelArray[2]) * convertAccel * 1000;
+							
+					}
+					xIntA = (int)xA;
+					yIntA = (int)yA;
+		          	zIntA = (int)zA;
+					
+					fufoCmd4LCD(LCD_CLEAR);
+					fufoDelayMs(10);
+					fufoOutputInt(dataAccelArray[0]);
+					fufoDelayMs(5);
+					fufoDat4LCD(',');
+					fufoDelayMs(5);
+					fufoOutputInt(dataAccelArray[1]);
+					fufoDelayMs(5);
+					fufoDat4LCD(',');
+					fufoDelayMs(5);
+					fufoOutputInt(dataAccelArray[2]);
+		            fufoDelayMs(5);
+		
+					fufoCmd4LCD(LCD_HOMEL2);
+					fufoDelayMs(10);
+					if(dataAccelArray[0] >= 4096) { 
+						fufoDat4LCD('-');
+						fufoDelayMs(5);
+					}				
+					fufoOutputInt(xIntA);
+					fufoDelayMs(5);
+					fufoDat4LCD(',');
+					fufoDelayMs(5);
+					if(dataAccelArray[1] >= 4096) {
+						fufoDat4LCD('-');
+						fufoDelayMs(5);
+					}
+					fufoOutputInt(yIntA);
+					fufoDelayMs(5);
+					fufoDat4LCD(',');
+					fufoDelayMs(5);
+					if(dataAccelArray[2] >= 4096) { 
+						fufoDat4LCD('-');
+						fufoDelayMs(5);
+					}
+					fufoOutputInt(zIntA);
+		            fufoDelayMs(5);
+				} else {fufoOutputChar(noticeError);}
+			} else {
+				error = fufoReadGyro(dataGyroArray);
+				if(error == 0) {
+					if(dataGyroArray[0] < 32768) {
+						xG = dataGyroArray[0] * convertGyro * 1000;
+					} else {
+						xG = (65536 - dataGyroArray[0]) * convertGyro * 1000;
+					}
+	
+					if(dataGyroArray[1] < 32768) {
+						yG = dataGyroArray[1] * convertGyro * 1000;
+					} else {
+						yG = (65536 - dataGyroArray[1]) * convertGyro * 1000;
+					}
+	
+					if(dataGyroArray[2] < 32768) {
+						zG = dataGyroArray[2] * convertGyro * 1000;
+					} else {
+						zG = (65536 - dataGyroArray[2]) * convertGyro * 1000;
+					}
+	
+					xIntG = (int)xG;
+					yIntG = (int)yG;
+					zIntG = (int)zG;
+	
+					fufoOutputInt(dataGyroArray[0]);
+					fufoDelayMs(5);
+					fufoDat4LCD(',');
+					fufoDelayMs(5);
+					fufoOutputInt(dataGyroArray[1]);
+					fufoDelayMs(5);
+					fufoDat4LCD(',');
+					fufoDelayMs(5);
+					fufoOutputInt(dataGyroArray[2]);
+	            		fufoDelayMs(5);
+	
+					fufoCmd4LCD(LCD_HOMEL2);
+					fufoDelayMs(10);
+					if(dataGyroArray[0] >= 32768) {
+						fufoDat4LCD('-');
+						fufoDelayMs(5);
+					}
+					fufoOutputInt(xIntG);
+					fufoDelayMs(5);
+					fufoDat4LCD(',');
+					fufoDelayMs(5);
+					if(dataGyroArray[1] >= 32768) {
+						fufoDat4LCD('-');
+						fufoDelayMs(5);
+					}
+					fufoOutputInt(yIntG);
+					fufoDelayMs(5);
+					fufoDat4LCD(',');
+					fufoDelayMs(5);
+					if(dataGyroArray[2] >= 32768) {
+						fufoDat4LCD('-');
+						fufoDelayMs(5);
+					}
+					fufoOutputInt(zIntG);
+	            		fufoDelayMs(5);
+				} else {fufoOutputChar(noticeError);}
+			}
+			fufoDelayMs(500);
+//			error = fufoReadByteI2C(L3G4200D_I2C, 0x0F, dataArr);
+//			if(error == 0) {
+//				if(dataArr[0] == 0xD3) {
+//					fufoDat4LCD('D');
+//				} else {fufoDat4LCD('S');}
+//			} else {fufoOutputChar(noticeError);}
+//			fufoDelayMs(500);			
         }
 }
 
@@ -73,15 +180,7 @@ void fufoInitSystem() {
 	error = fufoInitAccel();
 	fufoDelayMs(1000);
 	error = fufoInitGyro();
-	fufoDelayMs(1000);
-	if(error != 0) {
-        fufoDelayMs(1000);
-		_LATE0 = 1;
-	}
-
-	
-	
-	fufoDelayMs(50);
+	fufoDelayMs(1000);	
 	fufoInitLCDPorts();
 	fufoDelayMs(5);
 	fufoInitLCD();
