fe312acfe462d60c21f8bc48923dccef5be698d5
hunghd60115@gmail.com <hunghd60115@gmail.com@80728436-8353-cb08-c207-d9da5290a688>
2012-06-18 03:42:12 +0000

    update code for FUFO

--- SourceCode/FUFO/Control/Control.h
+++ SourceCode/FUFO/Control/Control.h
@@ -5,10 +5,23 @@
 #include "../UART/UART.h"
 #include "../PWM/pwmControl.h"
 
-void ControlFUFO();
+#define KpPhi 		0
+#define KdPhi		0
+#define KiPhi		0
+#define KpTheta		0
+#define KdTheta		0
+#define KiTheta		0
+#define KpPsi		0
+#define KdPsi		0
+#define KiPsi		0
+
+void controlFUFO(void);
 void getInstruction(void);
+int getThrustRate(void);
 void getValuefromBluetooth(void);
 void setSetpoint(unsigned int phi, unsigned int theta, unsigned int psi);
-void getValuefromSensor(unsigned int phiDesire, unsigned int thetaDesire, unsigned int psiDesire);
+void getValuefromSensor();
+void calcAngle_sum(unsigned int phiDesire, unsigned int thetaDesire, unsigned int psiDesire);
+int calcAngle(unsigned int desire_Angle, unsigned int actual_Angle, unsigned int Kp, unsigned int Ki, unsigned int Kd);
 void resetInstruction(void);
-void resetAngle(void);
+void resetSensor(void);
--- SourceCode/FUFO/Main.c
+++ SourceCode/FUFO/Main.c
@@ -6,6 +6,8 @@
 #include "UART/UART.h"
 #include "Control/Control.h"
 #include "math.h"
+#include "FUFO.h"
+
 
 	_FOSC(CSW_FSCM_OFF & XT);		//Clock = 7.3728 MHz
 	_FWDT(WDT_OFF);
@@ -14,45 +16,75 @@
 
 //------------------------------------------------------------------------------
 //Cac hang so cua chuong trinh (gia tri tuc thoi dung trong chuong trinh)
-#define Fcy		(7372800/4)			//Tan so thuc thi lenh
+unsigned int userInput = 1;
 
 //Cac prototype cho cac chuong trinh con
 void initFUFO(void);
 
 //Cac bien toan cuc
 unsigned char receiOK[25];
+unsigned int index = 0;
+
 
 //------------------------------------------------------------------------------
 //Chuong trinh chinh
 int main(void) {
 	initFUFO();
+	fufoCmd4LCD(LCD_CLEAR);
+	fufoOutputChar("Welcome to FUFO");
+	fufoDelayMs(300);
 	while(1){
 		fufoCmd4LCD(LCD_CLEAR);
-		fufoDelayMs(50);
-		fufoOutputInt(PDC1);
-		fufoOutputInt(PDC2);
-		fufoDelayMs(50);
+		fufoOutputChar("Thrust Rate: ");
+		fufoOutputInt(getThrustRate());
 		fufoCmd4LCD(LCD_HOMEL2);
-		fufoOutputInt(PDC3);
-		fufoOutputInt(PR4);
-		fufoDelayMs(500);
-		ControlFUFO();
-		
+		fufoOutputInt(PDC2);
+		if (userInput == 1){
+			getInstruction();
+		}
 	}
 }
 
 //Ham khoi tao cac chuong trinh cua FUFO
 void initFUFO(void){
-	initPWM(Fcy, 1843);		// khoi tao PWM voi tan so thuc thi lenh Fcy;
-							// do rong xung ban dau 1ms (1843).
-	fufoDelayMs(500);
-	fufoInitUART();
-	fufoDelayMs(10);
 	fufoInitLCDPorts();
 	fufoDelayMs(10);
 	fufoInitLCD();
 	fufoDelayMs(10);
-	fufoInitBluetooth(receiOK);
-	testConnectBluetooth(receiOK);
+	fufoOutputChar("Please wait");
+	fufoCmd4LCD(LCD_HOMEL2);
+	initPWM(1843);		// khoi tao PWM voi tan so thuc thi lenh Fcy;
+							// do rong xung ban dau 1ms (1843).
+	fufoDelayMs(200);
+	fufoOutputChar("...");
+	fufoDelayMs(500);
+	fufoOutputChar("....");
+	fufoInitUART();
+	fufoDelayMs(200);
+	fufoOutputChar("....");
+//	fufoInitBluetooth(receiOK);
+//	testConnectBluetooth(receiOK);
 }
 
+void initTMR2(void){
+	IPC1bits.T2IP = 6;  	//highest priority interrupt
+	T2CONbits.TCKPS = 0;	// timer 2 prescale = 1
+	TMR2 = 0;
+	PR2 = Fcy/Fsp;		
+	IFS0bits.T2IF = 0;		//interupt flag clear
+    IEC0bits.T2IE = 1;  	//Enable Timer1 Interrupt Service Routine
+	T2CONbits.TON = 0;		//timer 2 off
+}
+
+void __attribute__((__interrupt__ , auto_psv)) _T2Interrupt (void)
+{	
+	TMR2 = 0;			//reset timer register
+	controlFUFO();
+	index++;
+	userInput = 0;
+	if (index == 25){
+		index = 0;
+		userInput = 1;
+	}
+	IFS0bits.T2IF = 0;	// clear interrupt flag manually
+}
