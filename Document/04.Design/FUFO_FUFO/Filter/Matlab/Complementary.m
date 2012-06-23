106c5f43c3ea949c329834d46307c8caf4786dbe
phongvm90@gmail.com <phongvm90@gmail.com@80728436-8353-cb08-c207-d9da5290a688>
2012-06-23 04:02:24 +0000

    add lowpass filter and high pass filter

--- Document/04.Design/FUFO_FUFO/Filter/Matlab/Complementary.m
+++ Document/04.Design/FUFO_FUFO/Filter/Matlab/Complementary.m
@@ -2,18 +2,40 @@ load D:\status4.txt;         %  read data into status
 step = status4(:,1);     % Steps
 gyrosDOT = status4(:,2);    %  first column into gyrosDOT value
 accel = status4(:,3);    %  second column into accel value (Already in Euler Angle)
+
 % the angles calculated by gyrosDOT 
 gyrosAngle = gyrosDOT*0.01;  % 0.01 is the delta t
 gyros = cumsum(gyrosAngle);   
+
 % Complementary Filter
 k = 0.98;  % Time Coefficient
 numOfSample = length(gyrosDOT); 
-ComplementedEuler = zeros(1,numOfSample); % create the array
-ComplementedEuler(1) = k*(0 + gyrosDOT(1)*0.01) + (1-k)*accel(1); % calculate the first Euler value
+ComplementaryEuler = zeros(1,numOfSample); % create the array
+ComplementaryEuler(1) = k*(0 + gyrosDOT(1)*0.01) + (1-k)*accel(1); % calculate the first Euler value
 for i = 2:numOfSample
-    ComplementedEuler(i) = k*(ComplementedEuler(i-1) + gyrosDOT(i)*0.01) + (1-k)*accel(i);  % complementary filter
+    ComplementaryEuler(i) = k*(ComplementaryEuler(i-1) + gyrosDOT(i)*0.01) + (1-k)*accel(i);  % complementary filter
 end
-plot(step,accel,'r-',step,gyros,'g-', step, ComplementedEuler,'b-');     %  plot 
+
+%low pass filter
+LowpassEuler = zeros(1,numOfSample); % Create the array
+LowpassEuler(1) = k*0 + (1-k)*accel(1); % calculate the first Euler value
+for i = 2:numOfSample
+    LowpassEuler(i) = k*LowpassEuler(i-1) + (1-k)*accel(i);
+end
+
+%high pass filter
+HighpassEuler = zeros(1,numOfSample); % Create the array
+HighpassEuler(1) = k*(0 + gyros(i)*0.01); % calculate the first Euler value
+for i = 2:numOfSample
+    % ? * y[i-1] + ? * (x[i] - x[i-1]) = ? * (y[i-1] + (x[i] - x[i-1]))
+    HighpassEuler(i) = k*(HighpassEuler(i-1) + gyros(i)*0.01); 
+end
+
+
+
+plot(step,accel,'r-',step,gyros,'g-', step, ComplementaryEuler,'b-',step, LowpassEuler,'c-',step, HighpassEuler,'m-');     %  plot 
 xlabel('sensor');              %  add axis labels and plot title
 ylabel('angles');
 title('Test');
+h = legend('accel','gyros','Complementary','Lowpass','Highpass',3);
+set(h,'Interpreter','none');
