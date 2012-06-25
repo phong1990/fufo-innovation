9c820bb6a41926ab449992301b5cd3b5e279352d
khoinguyen67 <khoinguyen67@80728436-8353-cb08-c207-d9da5290a688>
2012-06-25 09:27:07 +0000

    fix bug forces close on tab setting. add  video stream function to control tab

--- SourceCode/VP/AOP/src/aop/control/ControlActivity.java
+++ SourceCode/VP/AOP/src/aop/control/ControlActivity.java
@@ -18,18 +18,24 @@
  */
 package aop.control;
 
+import java.util.Iterator;
+import java.util.List;
+
+import aop.video.*;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.Size;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Toast;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TableLayout;
-import android.widget.TextView;
 
 /**
  * @author khoinguyen67
@@ -39,6 +45,7 @@ public class ControlActivity extends Activity implements OnCheckedChangeListener
     Button  bt_Fwd, bt_Bwd, bt_Rgt, 
     bt_Lft, bt_Up, bt_Dwn, bt_Nkdh, bt_Kdh, bt_Start;
    public static RadioButton rd_Computer,rd_Phone;
+   FrameLayout preview;
     RadioGroup rd_Group;
     TableLayout tb_LO;
     public void onCreate(Bundle savedInstanceState) {
@@ -58,6 +65,7 @@ public class ControlActivity extends Activity implements OnCheckedChangeListener
         rd_Computer = (RadioButton)findViewById(R.id.rd_computer);
         rd_Phone = (RadioButton)findViewById(R.id.rd_phone);
         tb_LO = (TableLayout)findViewById(R.id.tb_layout);
+        preview = (FrameLayout) findViewById(R.id.camera_preview);
         rd_Group.setOnCheckedChangeListener(this);
         bt_Fwd.setOnClickListener(Control.cmct);
         bt_Bwd.setOnClickListener(Control.cmct);
@@ -70,7 +78,7 @@ public class ControlActivity extends Activity implements OnCheckedChangeListener
         bt_Start.setOnClickListener(Control.cmct);
  
             tb_LO.setVisibility(4);
-
+            settingCamera();
         
     }
     View.OnClickListener testOnClick = new View.OnClickListener() {
@@ -96,7 +104,7 @@ public class ControlActivity extends Activity implements OnCheckedChangeListener
         Log.d("FUFO", "radio button pressed!");
         // TODO Auto-generated method stub
         try{
-          if(arg0.getCheckedRadioButtonId() == 0x7f060001){
+          if(arg0.getCheckedRadioButtonId() == R.id.rd_computer){
               Log.d("FUFO", "computer radio button pressed!");
           tb_LO.setVisibility(4); 
           Log.d("FUFO", "computer radio button pressed1!");
@@ -117,4 +125,116 @@ public class ControlActivity extends Activity implements OnCheckedChangeListener
         
     }
 }
+    
+    
+    
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    
+    public void settingCamera(){
+        if (checkCameraHardware(this)) {
+            //,// Create an instance of Camera
+            mCamera = getCameraInstance();
+            Log.d("aop", "da vao");
+            // Create our Preview view and set it as the content of our
+            // activity.
+            mPreview = new CameraPreview(this, mCamera);
+            Log.d("aop", "da vao2");
+            
+            preview.addView(mPreview);
+            Log.d("aop", "da vao3");
+
+            Log.d("aop", "da vao311");
+            // get Camera parameters
+            Camera.Parameters params = mCamera.getParameters();
+            Log.d("aop", "da vao312");
+  /*          List<int[]> supportedPreviewFps = params
+                    .getSupportedPreviewFpsRange();
+            Iterator<int[]> supportedPreviewFpsIterator = supportedPreviewFps
+                    .iterator();
+            Log.d("aop", "da vao31");
+            while (supportedPreviewFpsIterator.hasNext()) {
+                int[] tmpRate = supportedPreviewFpsIterator.next();
+                StringBuffer sb = new StringBuffer();
+                sb.append("supportedPreviewRate: ");
+                for (int i = tmpRate.length, j = 0; j < i; j++) {
+                    sb.append(tmpRate[j] + ", ");
+                }
+                Log.v("CameraTest", sb.toString());
+            }*/
+            Log.d("aop", "da vao32");
+            // List the supported Preview Sizes
+            List<Size> supportedPreviewSizes = params
+                    .getSupportedPreviewSizes();
+            Iterator<Size> supportedPreviewSizesIterator = supportedPreviewSizes
+                    .iterator();
+            Log.d("aop", "da vao33");
+            while (supportedPreviewSizesIterator.hasNext()) {
+                Size tmpSize = supportedPreviewSizesIterator.next();
+                Log.v("CameraTest", "supportedPreviewSize.width = "
+                        + tmpSize.width + "supportedPreviewSize.height = "
+                        + tmpSize.height);
+            }
+
+            // Just show the current framerate for fun
+            Log.v("CameraTest", "Camera PreviewFrameRate = "
+                    + mCamera.getParameters().getPreviewFrameRate());
+            Log.d("aop", "da vao4");
+            // Start preview
+            mCamera.startPreview();
+            Log.d("aop", "da vao5");
+            // Add a listener to the Capture button
+           
+        }
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // check if the system has a camera
+        if (checkCameraHardware(this)) {
+            // in case user forget to push the stop button
+            if (mCamera != null) {
+                // Stoppreview first
+                mCamera.stopPreview();
+                // release camera
+                releaseCamera();
+            }
+        }
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA)) {
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        } catch (Exception e) {
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    /** Release the camera **/
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.setPreviewCallback(null);
+            mCamera.release(); // release the camera for other applications
+            mCamera = null;
+        }
+    }
+    
+    
+    
+    
 }
--- SourceCode/VP/AOP/src/aop/video/CameraPreview.java
+++ SourceCode/VP/AOP/src/aop/video/CameraPreview.java
@@ -27,7 +27,7 @@ public class CameraPreview extends SurfaceView implements
     byte[] picBuffer;
     
     public Client client;
-	private SurfaceHolder mHolder;
+	public SurfaceHolder mHolder;
 	private Camera mCamera;
 	private String TAG = "CameraPreview.";
 	// This variable is responsible for getting and setting the camera settings
@@ -46,7 +46,6 @@ public class CameraPreview extends SurfaceView implements
 	
 	public CameraPreview(Context context, Camera camera) {
 		super(context);
-		Log.d("aop", "vao previewq");
 		mCamera = camera;
 
 		// Install a SurfaceHolder.Callback so we get notified when the
@@ -56,12 +55,11 @@ public class CameraPreview extends SurfaceView implements
 		// deprecated setting, but required on Android versions prior to 3.0
 		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 		 client = new Client();
-		 Log.d("aop", "tao client");
          client.creatClient();
-         Log.d("aop", "tao client xong");
 	}
 
-	public void surfaceCreated(SurfaceHolder holder) {
+	@Override
+    public void surfaceCreated(SurfaceHolder holder) {
 		// The Surface has been created, now tell the camera where to draw the
 		// preview.
 		try {
@@ -90,13 +88,15 @@ public class CameraPreview extends SurfaceView implements
 		}
 	}
 
-	public void surfaceDestroyed(SurfaceHolder holder) {
+	@Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
 		// Surface will be destroyed when we return, so stop the preview.
 		// Because the CameraDevice object is not a shared resource, it's very
 		// important to release it when the activity is paused.
 	}
 
-	public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+	@Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
 		// If your preview can change or rotate, take care of those events here.
 		// Make sure to stop the preview before resizing or reformatting it.
 
@@ -120,7 +120,11 @@ public class CameraPreview extends SurfaceView implements
 
 		// start preview with new settings
 		try {
-			mCamera.setPreviewDisplay(mHolder);
+		    SurfaceView view = new SurfaceView(getContext());
+		    mCamera.setPreviewDisplay(view.getHolder());
+		  //  mCamera.startPreview();
+		    
+			//mCamera.setPreviewDisplay(mHolder);
 			mCamera.startPreview();
 
 		} catch (Exception e) {
@@ -132,7 +136,8 @@ public class CameraPreview extends SurfaceView implements
 
 	@Override
 	public void onPreviewFrame(byte[] data, Camera camera) {
-		Log.v("CameraTest", "Time Gap = "
+	    Log.d("aop", "da vao6");
+		Log.d("FUFO", "Time Gap = "
 				+ (System.currentTimeMillis() - timestamp));
 		timestamp = System.currentTimeMillis();
 		// NV21toRGB24Converter(pixels, data, previewSize.width,
@@ -154,11 +159,130 @@ public class CameraPreview extends SurfaceView implements
 		// Write the result in to file
 		// writeToFile(buffer);
 		 //Sendfile to Server
-        Log.v("CameraTest", "package = "
+        Log.d("FUFO", "package = "
                 + fileNumber);
 	   
 	       
 		    client.sendPackage(buffer);
 
+	}
+	
+	/**
+	 * This function accept a byte array and write it into a file.
+	 * The file name = filename + fileNumber.
+	 * The result directory = directory for picture in the file system.
+	 */
+	public void writeToFile(byte[] array) {
+		try {
+			// Log.d("file path", path);
+			File root = Environment
+					.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+			File fileToWrite = new File(root, filename + fileNumber+".jpeg");
+			root.mkdirs();
+			if (root.canWrite()) {
+				Log.i("root", root.getAbsolutePath());
+				OutputStream os = new FileOutputStream(fileToWrite);
+				Log.i("path", fileToWrite.getAbsolutePath());
+				os.write(array);
+				os.close();
+			}
+
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Convert NV21 to RGB24. This function use the method in Avery Lee's JFIF
+	 * Clarification
+	 */
+	void NV21toRGB24Converter(int[] rgb, byte[] NV21, int width, int height) {
+
+		// frameSize in pixel = width * height
+		final int frameSize = width * height;
+		// run j from 0 to height. This j value will count the current row.
+		// yp: is the Y value per pixel
+		for (int j = 0, yp = 0; j < height; j++) {
+			// j (or current row) will be divided by 2, and lower rounded.
+			// then after multiplied by width, result will add to the frame size
+			// to determine the next position
+			// of value V. (followed by value U)
+			int uvp = frameSize + (j >> 1) * width, u = 0, v = 0;
+			// increase yp to determine the next position of value Y.
+			for (int i = 0; i < width; i++, yp++) {
+				// read the byte Y, convert it into integer (by add 24 zeros
+				// bits to the left)
+				// then AND the result with 0000 0000 0000 0000 0000 0000 1111
+				// 1111.
+				// This process will make sure the value of Y is changed into
+				// integer with no
+				// damage.
+				int y = (0xff & (NV21[yp]));
+				if (y < 0)
+					y = 0;
+				// 1 = 0000 0000 0000 0000 0000 0000 0000 0001
+				// i increase from 0 to width, if i is odd, (i & 1) will not
+				// equal to 0;
+				// if i is even, (i & 1) will equal to 0;
+				if ((i & 1) == 0) {
+					// read byte V into integer then increase uvp. The method
+					// here is the same as
+					// reading byte Y above.
+					v = (0xff & NV21[uvp++]) - 128;
+					// read byte U into integer then increase uvp. The method
+					// here is the same as
+					// reading byte Y above.
+					u = (0xff & NV21[uvp++]) - 128;
+				}
+
+				// the following code is just algorithm in YUV to RGB
+				// conversion.
+				int r = (int) (y + 1.402f * v);
+				int g = (int) (y - 0.344f * u - 0.714f * v);
+				int b = (int) (y + 1.772f * u);
+				// limit the value of R, G and B in the following inteval:
+				// 0000 0000 0000 0000 0000 0000 0000 0000 <= r|g|b <= 0000 0000
+				// 0000 0000 0000 0000 1111 1111
+				// Reason is:
+				// 1. for R value, when we shift 16 bits to the left (code
+				// below) we can get 8 MSB of this
+				// value, then discard the rest. The maximum value of these 8
+				// MSB can be 1111 1111 or 255.
+				// 2. for G value, when we shift 8 bits to the left, we can also
+				// get the 8 MSB of this value
+				// just like the case of R value.
+				// 3. for B value, to get 8 MSB of it, we don't have to shift
+				// any thing..
+				//
+				// This interval accept the loss of any value that above
+				// maximum.
+				if (r < 0)
+					r = 0;
+				else if (r > 255)
+					r = 255;
+				if (g < 0)
+					g = 0;
+				else if (g > 255)
+					g = 255;
+				if (b < 0)
+					b = 0;
+				else if (b > 255)
+					b = 255;
+
+				// the following formula will construct a pixel of RGB
+				// in the following structure of hex number: aa rr gg bb
+				// aa: is the alpha bits, in this case it is always equal to
+				// 'ff'
+				// rr: Red bits (8 MSB of the R value)
+				// gg: Green bits (8 MSB of the G value)
+				// bb: Blue bits (8 MSB of the B value)
+				rgb[yp] = 0xff000000 | ((r << 16) & 0xff0000)
+						| ((g << 8) & 0xff00) | (b & 0xff);
+			}
+		}
 	}
 }
