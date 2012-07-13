d701fcf8d417b308fb28cb987b6b38ef276491c4
hunghd60115@gmail.com <hunghd60115@gmail.com@80728436-8353-cb08-c207-d9da5290a688>
2012-07-13 02:34:27 +0000

    update source code test PID: test roll(theta Angle)

--- SourceCode/FUFO/Control/Control.h
+++ SourceCode/FUFO/Control/Control.h
@@ -2,9 +2,9 @@
 #define KpPhi 		0
 #define KdPhi		0
 #define KiPhi		0
-//#define KpTheta		0.727
+//#define KpTheta		0.01
 #define KdTheta		0
-#define KiTheta		0	//0.424
+#define KiTheta		0
 #define KpPsi		0
 #define KdPsi		0
 #define KiPsi		0
@@ -21,8 +21,7 @@ int getUserInput(void);
 void setSetpoint(float phi, float theta, float psi);
 void getValuefromSensor();
 void calcAngle_sum(float phiDesire, float thetaDesire, float psiDesire);
-float calcRollAngle(float desireR_Angle, float actualR_Angle, float KpR, float KiR, float KdR);
-float calcPitchAngle(float desireP_Angle, float actualP_Angle, float KpP, float KiP, float KdP);
+float calcRollAngle(float desire_Angle, float actual_Angle, float KpR, float KiR, float KdR);
 void resetInstruction(void);
 void resetSensor(void);
 void resetPWM(void);
