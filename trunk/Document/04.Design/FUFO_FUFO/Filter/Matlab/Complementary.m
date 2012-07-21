f86ce5645aa7e4299ee67fe72879392a143bb2cd
phongvm90@gmail.com <phongvm90@gmail.com@80728436-8353-cb08-c207-d9da5290a688>
2012-07-21 09:37:36 +0000

    high pass filter

--- trunk/Document/04.Design/FUFO_FUFO/Filter/Matlab/Complementary.m
+++ trunk/Document/04.Design/FUFO_FUFO/Filter/Matlab/Complementary.m
@@ -1,14 +1,14 @@
 load D:\statusyphi.txt;         %  read data into status
 step = statusyphi(:,1);     % Steps
-gyrosDOT = statusyphi(:,2);    %  first column into gyrosDOT value
-accel = statusyphi(:,3);    %  second column into accel value (Already in Euler Angle)
+gyrosDOT = statusyphi(:,3);    %  first column into gyrosDOT value
+accel = statusyphi(:,2);    %  second column into accel value (Already in Euler Angle)
 
 % the angles calculated by gyrosDOT 
 gyrosAngle = gyrosDOT*0.01;  % 0.01 is the delta t
 gyros = cumsum(gyrosAngle);   
 
 % Complementary Filter
-k = 0.98;  % Time Coefficient
+k = 0.99;  % Time Coefficient
 numOfSample = length(gyrosDOT); 
 ComplementaryEuler = zeros(1,numOfSample); % create the array
 ComplementaryEuler(1) = k*(0 + gyrosDOT(1)*0.01) + (1-k)*accel(1); % calculate the first Euler value
@@ -28,7 +28,7 @@ HighpassEuler = zeros(1,numOfSample); % Create the array
 HighpassEuler(1) = k*(0 + gyros(i)*0.01); % calculate the first Euler value
 for i = 2:numOfSample
     % ? * y[i-1] + ? * (x[i] - x[i-1]) = ? * (y[i-1] + (x[i] - x[i-1]))
-    HighpassEuler(i) = k*(HighpassEuler(i-1) + gyros(i)*0.01); 
+    HighpassEuler(i) = k*HighpassEuler(i-1) + k*(gyros(i)-gyros(i-1)); 
 end
 
 
