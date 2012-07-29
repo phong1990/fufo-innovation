c87d0ce95cc4a59f0f6ee7a69e9d92a6c5f472a6
hunghd60115@gmail.com <hunghd60115@gmail.com@80728436-8353-cb08-c207-d9da5290a688>
2012-07-29 16:38:48 +0000

    bo set point, PID gần như là ok :)

--- SourceCode/FUFO/Control/Control.h
+++ SourceCode/FUFO/Control/Control.h
@@ -1,10 +1,10 @@
 
-#define KpPhi 		8.9		//1.08
-#define KdPhi		3.19	//1.0163
-#define KiPhi		2.19	//1.3481
-#define KpTheta		8.9		//1.08		//9
-#define KdTheta		3.19	//1.0163	//3.29
-#define KiTheta		2.19	//1.3481 	//6.155
+#define KpPhi 		19		//8.9		//1.08
+#define KdPhi		5.8		//3.19	//1.0163
+#define KiPhi		18		//2.19	//1.3481
+#define KpTheta		19		//1.08		//9
+#define KdTheta		5.8		//1.0163	//3.29
+#define KiTheta		18	//1.3481 	//6.155
 #define KpPsi		8.9
 #define KdPsi		3.19
 #define KiPsi		2.19
@@ -24,6 +24,7 @@ void calcAngle_sum(float phiDesire, float thetaDesire, float psiDesire);
 float calcRollAngle(float desireR_Angle, float actualR_Angle, float Gyros_OutputR, float KpR, float KiR, float KdR);
 float calcPitchAngle(float desireP_Angle, float actualP_Angle, float Gyros_OutputP, float KpP, float KiP, float KdP);
 float calcYawAngle(float desireY_Angle, float actualY_Angle, float Gyros_OutputY, float KpY, float KiY, float KdY);
+float calcAltitude(float desireAltitude, float actualAltitude, float KpA, float KiA, float KdA);
 void resetInstruction(void);
 void resetSensor(void);
 void resetPWM(void);
